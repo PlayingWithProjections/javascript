e01d60d3e6751a3de0c79f17c50089f2c3ebe701
Michel Grootjans <michel.grootjans@persgroep.net>
2019-09-06 09:28:42 +0200

    allow big source files

--- src/EventStore.js
+++ src/EventStore.js
@@ -1,23 +1,51 @@
+// const fs = require('fs');
+//
+// module.exports = function (...projections) {
+//   const mapTimestamp = event => ({...event, timestamp: new Date(event.timestamp)});
+//
+//   const replay = filePath => {
+//     console.log(`reading events from ${filePath} ...`);
+//     let text = fs.readFileSync(filePath);
+//
+//     console.log('parsing events...');
+//     let events = JSON.parse(text);
+//
+//     console.log('replaying events...');
+//     events
+//       .map(mapTimestamp)
+//       .forEach(e =>
+//         projections.forEach(projection => projection(e))
+//       );
+//   };
+//
+//   return {replay}
+// };
+
+const StreamArray = require('stream-json/streamers/StreamArray');
+const path = require('path');
 const fs = require('fs');
 
 module.exports = function (...projections) {
   const mapTimestamp = event => ({...event, timestamp: new Date(event.timestamp)});
 
-  const replay = filePath => {
+  const replay = (filePath, callback) => {
+
+    const jsonStream = StreamArray.withParser();
+
+    jsonStream.on('data', ({key, value}) => {
+      // console.log({key, value})
+      projections.forEach(projection => projection(value))
+    });
+    jsonStream.on('end', () => {
+      callback();
+    });
+
     console.log(`reading events from ${filePath} ...`);
-    let text = fs.readFileSync(filePath);
-
-    console.log('parsing events...');
-    let events = JSON.parse(text);
-
-    console.log('replaying events...');
-    events
-      .map(mapTimestamp)
-      .forEach(e =>
-        projections.forEach(projection => projection(e))
-      );
+    fs.createReadStream(filePath).pipe(jsonStream.input);
   };
 
   return {replay}
 };
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const EventStore = require('./EventStore');
 
-const getFileName = () => process.argv.slice(2)[0];
-
 const CountEvents = function () {
   let counter = 0;
-
   return {
     projection: e => counter++,
     result: () => {return counter }
   }
 };
 
+let fileName = process.argv.slice(2)[0];
 let projector = new CountEvents();
 
-new EventStore(projector.projection).replay(getFileName());
-
-console.log('number of events:', projector.result());
+new EventStore(projector.projection).replay(fileName, () => {
+  console.log('number of events:', projector.result());
+});
